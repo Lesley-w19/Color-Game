2db160e270a672a5d62a4ad64a316b8a235795ec
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-06-28 22:54:06 +0300

    Assignment 4 -Color Game

--- assets/index.js
+++ assets/index.js
@@ -6,20 +6,36 @@ var colorsBlocks;
 var rgbEl = document.querySelector(".rgb");
 var statusEl = document.querySelector(".status");
 var colors = [];
-createBlocks(n);
-resetGame();
+let number_of_tiles;
+let baseColor;
+resetGame(n);
 
-function checkColors(e) {
+function checkColors(color1, clickedDiv) {
   // your code here
+  if (color1 !== baseColor) {
+    clickedDiv.style.backgroundColor = "transparent";
+  } else {
+    colorsBlocks.forEach((colorBlock) => {
+      if (colorBlock < 2) {
+        document.querySelector(".text").innerHTML = "You have won!";
+        setNumberOfTiles();
+      } else {
+        colorBlock.style.backgroundColor = baseColor;
+        document.querySelector(".text").innerHTML = "You have won!";
+        setNumberOfTiles();
+      }
+    });
+  }
 }
 
-function resetGame() {
+function resetGame(n) {
   createBlocks(n);
   document.body.style.color = "black";
   colors = [];
-  pickColors();
+  pickColors(n);
   pickedColor = random(n);
-  rgbEl.innerHTML = colors[pickedColor];
+  baseColor = colors[pickedColor];
+  rgbEl.innerHTML = baseColor;
   setColors();
   statusEl.innerHTML =
     "Try to guess the right color based on the RGB value by clicking on the blocks.";
@@ -31,7 +47,7 @@ function setColors() {
   }
 }
 
-function pickColors() {
+function pickColors(n) {
   for (var i = 0; i < n; i++) {
     colors.push(randomColor());
   }
@@ -45,8 +61,12 @@ function random(r) {
   return Math.floor(Math.random() * r);
 }
 
-function setNumberOfTiles(e) {
+function setNumberOfTiles() {
   // your code here
+  setTimeout(() => {
+    resetGame(n);
+    document.querySelector(".text").innerHTML = "";
+  }, 1000);
 }
 
 function createBlocks(num) {
@@ -59,7 +79,28 @@ function createBlocks(num) {
     colorsEl.appendChild(block);
   }
   colorsBlocks = document.querySelectorAll(".colors__block");
-  for (var i = 0; i < colorsBlocks.length; i++) {
-    colorsBlocks[i].addEventListener("click", checkColors);
-  }
+
+  colorsBlocks.forEach((cBlock) => {
+    cBlock.addEventListener("click", (e) => {
+      const divAccessed = e.target;
+      const val = e.target.style.backgroundColor;
+      checkColors(val, divAccessed);
+    });
+  });
+
+  // for (var i = 0; i < colorsBlocks.length; i++) {
+  //   colorsBlocks[i].addEventListener("click", checkColors());
+  // }
 }
+
+diffEls.forEach((displayBtn) => {
+  displayBtn.addEventListener("click", (e) => {
+    number_of_tiles = e.target.innerHTML;
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    createBlocks(number_of_tiles);
+    random(number_of_tiles);
+    pickColors(number_of_tiles);
+    resetGame(number_of_tiles);
+  });
+});
